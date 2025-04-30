366575002e7085860554fc7ad9f09ddaf926c72a
Md Abdullah All Shamem <121669301+abdullah107189@users.noreply.github.com>
2025-04-30 20:04:38 +0600

    Update index.js
    set process.env.PORT

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const moment = require('moment-timezone');
 // const jwt = require("jsonwebtoken");
 // const cookieParser = require("cookie-parser");
 const app = express();
-const port = 3000;
+const port =process.env.PORT || 3000;
 
 const cors = require('cors');
 const http = require('http');
